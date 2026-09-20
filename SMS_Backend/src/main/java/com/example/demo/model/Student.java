7a98384cf95b14fa343d085b97d42105c77ea3b4
Parambrata Moitra <parambratamoitra@gmail.com>
2026-09-21 00:39:56 +0530

    Annotation Changes in Student.java

--- SMS_Backend/src/main/java/com/example/demo/model/Student.java
+++ SMS_Backend/src/main/java/com/example/demo/model/Student.java
@@ -101,6 +101,8 @@ public class Student {
 	public void setMustChangePassword(boolean mustChangePassword) {
 		this.mustChangePassword = mustChangePassword;
 	}
+	
+	//override toString() method
 	@Override
 	public String toString() {
 		return "Student [studentId=" + studentId + ", name=" + name + ", phoneNumber=" + phoneNumber + ", email="
@@ -109,7 +111,7 @@ public class Student {
 				+ passwordHash + ", mustChangePassword=" + mustChangePassword + "]";
 	}
 	
-	//override toString() method
+	
 	
 	
 	
